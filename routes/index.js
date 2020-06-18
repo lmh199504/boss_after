7c90c010fd514c1321d94a277d8e959862be78aa
lmh <207374233@qq.com>
2020-06-18 23:06:17 +0800

    登录

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const md5 = require('blueimp-md5')
 const {UserModel} = require('../db/models')
 
+const filter = { password:0,__v:0 }  //指定过滤的属性
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -29,6 +31,28 @@ router.post('/register',function (req,res,next) {
   })
   //返回响应数据
 
+})
+
+router.post('/login',function (req,res,next) {
+  const {username,password} = req.body
+  if(username === ''){
+    res.send({code:1,msg:'用户名不能为空'})
+  }else if(password === ''){
+    res.send({code:1,msg:'请输入密码'})
+  }else {
+    UserModel.findOne({username,password:md5(password)},filter,function (err,data) {
+      if(err){
+        res.send({code:1,msg:"用户不存在"})
+      }else{
+        if(data){
+          res.cookie('userid',data._id,{maxAge:1000*60*60*24})
+          res.send({code:0,data:data})
+        }else{
+          res.send({code:1,msg:"用户名或密码不正确."})
+        }
+      }
+    })
+  }
 })
 
 module.exports = router;

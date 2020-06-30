74a761215aa0e64d0814a73124f7afe8007fa221
lmh <207374233@qq.com>
2020-06-30 18:06:37 +0800

    2020-06-30 用户列表

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.post('/register',function (req,res,next) {
     }
 
     if(data){
-      res.send({code:1,msg:"此用户以存在"})
+      res.send({code:1,msg:"此用户已存在"})
     }else{
       new UserModel({password:md5(password),username,type}).save(function (error,saveData) {
         console.log(saveData)
@@ -67,28 +67,60 @@ router.post('/login',function (req,res,next) {
 
 router.post('/updata',function (req,res,next) {
   //从请求cookie 中获取userid
-
+  console.log(req.cookies)
   const userid = req.cookies.userid
   if(!userid){
     return res.send({code:1,msg:"请先登录."})
   }
   //存在userid，跟新数据库中对于id 的数据
   const user = req.body
-  UserModel.findByIdAndDelete({_id:userid},user,function (error,oldUser) {
-    if(!error){
+  UserModel.findByIdAndUpdate({_id:userid},user,function (error,oldUser) {
+
+    if(error){
+      console.log('errpr')
+      console.log(error)
       //通知浏览器删除userid 的cookie
       res.clearCookie('userid')
       res.send({code:1,msg:'请先登录。'})
     }else {
-
+      console.log('success')
+      console.log(oldUser)
       const { _id,username,type } = oldUser
       const data = Object.assign(user,{ _id,username,type })
-      res.send({code:1,data})
+      res.send({code:0,data})
     }
   })
 
 })
 
+
+router.get('/user',function (req,res,next) {
+  const userid = req.cookies.userid
+  if(!userid){
+    return res.send({code:1,msg:'请先登陆.'})
+  }
+  //根据userid查询对应的user
+  UserModel.findOne({_id:userid},filter,function (error,user) {
+    if(error){
+      return res.send({code:1,msg:'用户不存在'})
+    }
+    res.send({code:0,data:user})
+  })
+})
+
+router.get('/userlist',function (req,res,next) {
+
+  console.log(req.query)
+  const { type } = req.query
+
+  UserModel.find({type,header:{$exists: true}},filter,function (error,users) {
+    if(error){
+      return res.send({code:1,msg:'系统错误'})
+    }
+    res.send({code:0,data:users})
+
+  })
+})
 
 router.post('*',function (req,res,next) {
   console.log(req,res)
